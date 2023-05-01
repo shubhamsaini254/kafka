b4525f8aecf0ea8d238e21d645f54a02b4dbb2d1
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-05-01 18:46:43 +0530

    Changing in Access port file for add more info

--- code/2-kafka-operations/1-update-config/0-using-rolling-restart.sh
+++ code/2-kafka-operations/1-update-config/0-using-rolling-restart.sh
@@ -9,4 +9,7 @@ echo "min.insync.replicas=2" >> /home/ec2-user/kafka.properties
 #### ADMIN MACHINE
 ########
 # roll restart kafka
+
+kafka-rolling-restart --cluster-type kafka #This is in video so i typed here
+
 kafka-rolling-restart --cluster-type kafka --start-command "systemctl start kafka" --stop-command "systemctl stop kafka" --check-count 3
