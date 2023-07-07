d607ac1cbbbdcec9eb541af62258758d9b7cf5d4
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-07-07 15:21:23 +0530

    Create commands.sh

---
+++ KRAFT/commands.sh
@@ -0,0 +1,16 @@
+# This is admin commands for manage or get data about cluster of Kraft
+
+# Create UUID (Cluster ID) then copy UUID for future use
+bin/kafka-storage.sh random-uuid
+
+# Format the logs directory
+bin/kafka-storage.sh format -t <enter uuid here> -c config/kraft/server.properties
+
+# Login into Kraft shell
+bin/kafka-metadata-shell.sh --snapshot /tmp/server-1/kraft-combined-logs/__cluster_metadata-0/00000000000000000000.log
+
+# Check cluster status
+bin/kafka-metadata-quorum.sh --bootstrap-server 172.31.8.57:9092 describe --status
+
+# Check replication staus
+bin/kafka-metadata-quorum.sh --bootstrap-server 172.31.8.57:9092 describe --replication
