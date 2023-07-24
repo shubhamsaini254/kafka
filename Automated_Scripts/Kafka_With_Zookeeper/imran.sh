e870e6d55caab95ee9e1a5314832f34245739dbf
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-07-24 15:58:45 +0530

    Add files via upload

---
+++ Automated_Scripts/Kafka_With_Zookeeper/imran.sh
@@ -0,0 +1,263 @@
+#!/usr/bin/bash
+
+#Take User Input For Package Manager
+read -p "Enter the package manager (yum or apt): " package_manager
+
+if [ "$package_manager" = "yum" ]; then
+    echo "Running: sudo yum install java"
+    sudo yum update -y
+    sudo yum install java-1.8.0-openjdk -y
+elif [ "$package_manager" = "apt" ]; then
+    echo "Running: sudo apt-get install openjdk-8-jdk"
+    sudo apt-get update -y
+    sudo apt-get install openjdk-8-jdk netstat -y
+else
+    echo "Invalid package manager. Please enter 'yum' or 'apt'."
+fi
+
+#Export Username
+export USERNAME=$(whoami)
+# Export Private IP to .bashrc File
+echo export EC2_PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) >> ~/.bashrc
+# Export Private IP to $EC2_PRIVATE_IP variable
+export EC2_PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+# Update Repository
+#sudo apt-get update -y
+# Install jdk, net-tools and tree package
+#sudo apt-get install openjdk-8-jdk net-tools tree -y
+# Download Kafka Binaries - check url if not working
+wget https://downloads.apache.org/kafka/3.5.0/kafka_2.13-3.5.0.tgz
+# Extract kafka Binary
+tar -xvf kafka_2.13-3.5.0.tgz
+# Delete Kafka binary
+rm kafka_2.13-3.5.0.tgz
+# Create soft link of kafka binaries to kafka
+ln -s kafka_2.13-3.5.0 kafka
+# Create Data Directory
+mkdir /home/$USERNAME/data
+# Create systemd file for zookeeper
+cat << EOF | sudo tee /etc/systemd/system/zookeeper.service > /dev/null
+[Unit]
+Description=zookeeper
+After=network.target
+
+[Service]
+# User=ec2-user
+# Group=ec2-user
+Environment="KAFKA_HEAP_OPTS=-Xmx256M -Xms128M"
+# Environment="KAFKA_OPTS=-javaagent:/home/$USERNAME/prometheus/jmx_prometheus_javaagent-0.3.1.jar=8080:/home/$USERNAME/prometheus/kafka-0-8-2.yml"
+ExecStart=/home/$USERNAME/kafka/bin/zookeeper-server-start.sh /home/$USERNAME/kafka/config/zookeeper.properties
+ExecStop=/home/$USERNAME/kafka/bin/zookeeper-server-stop.sh
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Create systemd file for Kafka
+cat << EOF | sudo tee /etc/systemd/system/kafka.service > /dev/null
+[Unit]
+Description=Kafka
+After=network.target zookeeper.service
+Requires=zookeeper.service
+
+[Service]
+# User=ec2-user
+# Group=ec2-user
+Environment="KAFKA_HEAP_OPTS=-Xmx256M -Xms128M"
+# Environment="KAFKA_OPTS=-javaagent:/home/$USERNAME/prometheus/jmx_prometheus_javaagent-0.3.1.jar=8080:/home/$USERNAME/prometheus/kafka-0-8-2.yml"
+#ExecStartPre=/bin/sleep 30
+ExecStart=/home/$USERNAME/kafka/bin/kafka-server-start.sh /home/$USERNAME/kafka/config/server.properties
+ExecStop=/home/$USERNAME/kafka/bin/kafka-server-stop.sh
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
+# Delete zookeeper.properties and server.properties
+sudo rm /home/$USERNAME/kafka/config/zookeeper.properties
+sudo rm /home/$USERNAME/kafka/config/server.properties
+# Create zookeeper.properties
+cat << EOF | tee /home/$USERNAME/kafka/config/zookeeper.properties > /dev/null
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# the directory where the snapshot is stored.
+dataDir=/home/$USERNAME/data/zookeeper
+# the port at which the clients will connect
+clientPort=2181
+# disable the per-ip limit on the number of connections since this is a non-production config
+maxClientCnxns=0
+# Disable the adminserver by default to avoid port conflicts.
+# Set the port to something non-conflicting if choosing to enable this
+admin.enableServer=false
+# admin.serverPort=8080
+
+#Custom-Configuration
+tickTime=2000
+initLimit=10
+syncLimit=5
+4lw.commands.whitelist=*
+EOF
+# Create server.properties
+cat << EOF | tee /home/$USERNAME/kafka/config/server.properties > /dev/null
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This configuration file is intended for use in ZK-based mode, where Apache ZooKeeper is required.
+# See kafka.server.KafkaConfig for additional details and defaults
+#
+
+############################# Server Basics #############################
+
+# The id of the broker. This must be set to a unique integer for each broker.
+broker.id=1
+
+############################# Socket Server Settings #############################
+
+# The address the socket server listens on. If not configured, the host name will be equal to the value of
+# java.net.InetAddress.getCanonicalHostName(), with PLAINTEXT listener name, and port 9092.
+#   FORMAT:
+#     listeners = listener_name://host_name:port
+#   EXAMPLE:
+#     listeners = PLAINTEXT://your.host.name:9092
+listeners=PLAINTEXT://$EC2_PRIVATE_IP:9092
+
+# Listener name, hostname and port the broker will advertise to clients.
+# If not set, it uses the value for "listeners".
+#advertised.listeners=PLAINTEXT://your.host.name:9092
+
+# Maps listener names to security protocols, the default is for them to be the same. See the config documentation for more details
+#listener.security.protocol.map=PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
+
+# The number of threads that the server uses for receiving requests from the network and sending responses to the network
+num.network.threads=3
+
+# The number of threads that the server uses for processing requests, which may include disk I/O
+num.io.threads=8
+
+# The send buffer (SO_SNDBUF) used by the socket server
+socket.send.buffer.bytes=102400
+
+# The receive buffer (SO_RCVBUF) used by the socket server
+socket.receive.buffer.bytes=102400
+
+# The maximum size of a request that the socket server will accept (protection against OOM)
+socket.request.max.bytes=104857600
+
+
+############################# Log Basics #############################
+
+# A comma separated list of directories under which to store log files
+log.dirs=/home/$USERNAME/data/kafka-logs
+
+# The default number of log partitions per topic. More partitions allow greater
+# parallelism for consumption, but this will also result in more files across
+# the brokers.
+num.partitions=1
+
+# The number of threads per data directory to be used for log recovery at startup and flushing at shutdown.
+# This value is recommended to be increased for installations with data dirs located in RAID array.
+num.recovery.threads.per.data.dir=1
+
+############################# Internal Topic Settings  #############################
+# The replication factor for the group metadata internal topics "__consumer_offsets" and "__transaction_state"
+# For anything other than development testing, a value greater than 1 is recommended to ensure availability such as 3.
+offsets.topic.replication.factor=1
+transaction.state.log.replication.factor=1
+transaction.state.log.min.isr=1
+
+############################# Log Flush Policy #############################
+
+# Messages are immediately written to the filesystem but by default we only fsync() to sync
+# the OS cache lazily. The following configurations control the flush of data to disk.
+# There are a few important trade-offs here:
+#    1. Durability: Unflushed data may be lost if you are not using replication.
+#    2. Latency: Very large flush intervals may lead to latency spikes when the flush does occur as there will be a lot of data to flush.
+#    3. Throughput: The flush is generally the most expensive operation, and a small flush interval may lead to excessive seeks.
+# The settings below allow one to configure the flush policy to flush data after a period of time or
+# every N messages (or both). This can be done globally and overridden on a per-topic basis.
+
+# The number of messages to accept before forcing a flush of data to disk
+#log.flush.interval.messages=10000
+
+# The maximum amount of time a message can sit in a log before we force a flush
+#log.flush.interval.ms=1000
+
+############################# Log Retention Policy #############################
+
+# The following configurations control the disposal of log segments. The policy can
+# be set to delete segments after a period of time, or after a given size has accumulated.
+# A segment will be deleted whenever *either* of these criteria are met. Deletion always happens
+# from the end of the log.
+
+# The minimum age of a log file to be eligible for deletion due to age
+log.retention.hours=168
+
+# A size-based retention policy for logs. Segments are pruned from the log unless the remaining
+# segments drop below log.retention.bytes. Functions independently of log.retention.hours.
+#log.retention.bytes=1073741824
+
+# The maximum size of a log segment file. When this size is reached a new log segment will be created.
+#log.segment.bytes=1073741824
+
+# The interval at which log segments are checked to see if they can be deleted according
+# to the retention policies
+log.retention.check.interval.ms=300000
+
+############################# Zookeeper #############################
+
+# Zookeeper connection string (see zookeeper docs for details).
+# This is a comma separated host:port pairs, each corresponding to a zk
+# server. e.g. "127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002".
+# You can also append an optional chroot string to the urls to specify the
+# root directory for all kafka znodes.
+zookeeper.connect=$EC2_PRIVATE_IP:2181
+
+# Timeout in ms for connecting to zookeeper
+zookeeper.connection.timeout.ms=18000
+
+
+############################# Group Coordinator Settings #############################
+
+# The following configuration specifies the time, in milliseconds, that the GroupCoordinator will delay the initial consumer rebalance.
+# The rebalance will be further delayed by the value of group.initial.rebalance.delay.ms as new members join the group, up to a maximum of max.poll.interval.ms.
+# The default value for this is 3 seconds.
+# We override this to 0 here as it makes for a better out-of-the-box experience for development and testing.
+# However, in production environments the default value of 3 seconds is more suitable as this will help to avoid unnecessary, and potentially expensive, rebalances during application startup.
+group.initial.rebalance.delay.ms=0
+EOF
+# Restart Daemon-Services
+sudo systemctl daemon-reload
+# Enable zookeeper and kafka service files
+sudo systemctl enable zookeeper kafka
+# Start zookeeper
+sudo systemctl start zookeeper
+# Pause next command for 10 seconds
+/bin/sleep 5
+# Start Kafka
+sudo systemctl start kafka
+# Check kafka and zookeeper status
+sudo systemctl status zookeeper kafka
+# This should reload ~/.bashrc, ~/.bash_aliases, etc. (I have used at last because after this command next commands will be stopped)
+#This will reload and add private IP of EC2 Instance from .bashrc file in $EC2_PRIVATE_IP variable
+exec bash
