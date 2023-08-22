49443e4e343f8141591c60ee9d4e6f984ef96c3d
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-08-22 16:24:18 +0530

    Create Java_Cryptograhic_Extension.sh

---
+++ SCRAM_Security/System_Related_Cryptographic_Issue/Java_Cryptograhic_Extension.sh
@@ -0,0 +1,10 @@
+# Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files 8 Download
+# This is JCE Framework
+# Original URL - https://www.oracle.com/in/java/technologies/javase-jce8-downloads.html
+
+# Stop zookeeper and kafka servers first and follow below steps
+
+# 1. Visit and Download JCE For Java 8 on all system or check this directory for this file
+https://www.oracle.com/in/java/technologies/javase-jce8-downloads.html
+
+# 2. 
